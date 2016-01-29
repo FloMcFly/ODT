6824db63f950c78884f918a567b621be14638c95
FloMcFly <florian.frank2@googlemail.com>
2016-01-29 22:52:12 +0100

    Minor changes

--- setup/update.cmd
+++ setup/update.cmd
@@ -3,6 +3,5 @@ rmdir source /S /Q
 mkdir output
 setup.exe /download configuration.xml
 setup.exe /packager configuration.xml output
-copy "%temp%\OfficeSetup.txt" o365update.log
 echo "Check %TEMP% for more logfiles"
 pause
