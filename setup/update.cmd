3f4d97352ad5ac10dc0585ffc74c43180a6a7c19
FloMcFly <florian.frank2@googlemail.com>
2016-01-30 13:51:57 +0100

    update.cmd

--- setup/update.cmd
+++ setup/update.cmd
@@ -2,6 +2,7 @@ cd C:\Users\Public\Downloads\ODT-master
 rmdir output /S /Q
 rmdir source /S /Q
 mkdir output
+mkdir source
 setup.exe /download configuration.xml
 setup.exe /packager configuration.xml output
 echo "Check %TEMP% for more logfiles"
