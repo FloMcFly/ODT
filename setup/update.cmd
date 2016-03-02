2e1269274707a7d1ec89ed0b32d60628d584965a
FloMcFly <florian.frank2@googlemail.com>
2016-03-02 13:29:41 +0100

    Update update.cmd
    Add Set-O365DeploymentConfig step

--- setup/update.cmd
+++ setup/update.cmd
@@ -6,4 +6,7 @@ mkdir source
 setup.exe /download configuration.xml
 setup.exe /packager configuration.xml output
 echo "Check %TEMP% for more logfiles"
+
+powershell -file Set-O365DeploymentConfig.ps1  %USERPROFILE%\Downloads\ODT-master\setup\output\AppVPackages\O365ProPlusRetail_de-de_x86_DeploymentConfig.xml
+
 pause
