617573d38f62f12b4cb3b2bf2804d149e7f5ff08
FloMcFly <florian.frank2@googlemail.com>
2016-03-02 13:13:01 +0100

    Add Set-O365DeploymentConfig.ps1
    PS script for automatic DeploymentConfig.xml changes

---
+++ setup/Set-O365DeploymentConfig.ps1
@@ -0,0 +1,31 @@
+﻿$input = "$PSScriptRoot\O365ProPlusRetail_de-de_x86_DeploymentConfig.xml"
+
+# Read xml file
+if (Test-Path $input){
+    $config_file = [xml] (Get-Content $input)
+}
+else{
+    Write-Host -ForegroundColor Red "$input not found"
+    Exit 1
+}
+
+# Disable Shortcuts
+$config_file.DeploymentConfiguration.UserConfiguration.Subsystems.Shortcuts.Enabled = 'false'
+
+# Disable FTA
+$config_file.DeploymentConfiguration.UserConfiguration.Subsystems.FileTypeAssociations.Enabled = 'false'
+
+# Add/Remove Registry Keys
+#$reg_keys = $config_file.DeploymentConfiguration.UserConfiguration.Subsystems.Registry.Include.Key
+
+# Disable Applications (based on Blacklist)
+$blacklist = @('[{AppVPackageRoot}]\Office16\GROOVE.EXE',`    '[{AppVPackageRoot}]\Office16\lync.exe',`    '[{AppVPackageRoot}]\Office16\MSOUC.EXE',`    '[{AppVPackageRoot}]\Office16\MSPUB.EXE',`    '[{AppVPackageRoot}]\Office16\OcPubMgr.exe',`    '[{AppVPackageRoot}]\Office16\OMSMAIN.DLL',`
+    '[{AppVPackageRoot}]\Office16\OUTLOOK.EXE')
+
+$config_file.DeploymentConfiguration.UserConfiguration.Applications.Application | ForEach-Object { if ($blacklist -contains $_.Id){ $_.Enabled = 'false' }}
+
+# Backup original xml file
+Copy-Item -Path $input -Destination "$input.bak"
+
+# Save new xml file
+$config_file.Save($input)
